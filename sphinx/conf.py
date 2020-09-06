467ad5fff61cbfa89c1e9f113639727eec42df9d
letianzj <letian.zj@gmail.com>
2020-09-05 23:46:32 -0400

    Read the Docs bug
    https://stackoverflow.com/questions/56336234/build-fail-sphinx-error-contents-rst-not-found

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -54,5 +54,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+master_doc = 'index'
+
 #source_parsers = {'.md': CommonMarkParser}
 #source_suffix = ['.rst', '.md']
